1f7884ac4c225fa1940bfd2020b02768ad76bb90
Elias Wassertheurer <eliaswa@edu.aau.at>
2024-03-09 21:49:17 +0100

    Implemented Feature 2.1, Setup Server Connection and build a simple Android App

--- app/src/main/java/com/example/se2_einzelphase_wassertheurer/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelphase_wassertheurer/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.se2_einzelphase_wassertheurer;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Socket client;
+    private BufferedWriter out;
+    private BufferedReader in;
+
+    private static final String ip = "se2-submission.aau.at";
+    private static final int port = 20080;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +38,86 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView tvResponse = findViewById(R.id.tvResponse);
+        EditText etnMatrikelnummer = findViewById(R.id.eTNMatrikelnummer);
+        Button btnSend = findViewById(R.id.btnSend);
+
+        // network running on separate thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startConnection(ip, port);
+            }
+        }).start();
+
+        // button "Abschicken"
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sidString = etnMatrikelnummer.getText().toString();
+                int sid = Integer.parseInt(sidString);
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String respMsg = sendStudentId(sid);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvResponse.setText(respMsg);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+    }
+
+    // handles closing the app
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopConnection();
+    }
+
+    // builds up connection
+    public void startConnection(String ip, int port) {
+        try {
+            client = new Socket(ip, port);
+            out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        } catch (Exception e) {
+            Log.e("Error", "Exception: " + e + " thrown, when trying to start the connection.");
+        }
+        Log.i("Info", "Client connected to server on ip " + ip + ":" + port + ".");
+    }
+
+    // sends the studentId to the server
+    public String sendStudentId(int sid) {
+        String response = "";
+        try {
+            // sending the student Id to Server
+            out.write(Integer.toString(sid));
+            out.newLine(); // in.readLine() on server side
+            out.flush(); // "flushes" (sends) the data immediately
+            // return the response from the Server
+            response = in.readLine();
+        } catch(Exception e) {
+            Log.e("Error", "Exception: " + e + " thrown, when trying to send a message.");
+        }
+        return response;
+    }
+
+    // closes the connection
+    public void stopConnection() {
+        try {
+            in.close();
+            out.close();
+            client.close();
+        } catch (Exception e) {
+            Log.e("Error", "Exception: " + e + " thrown, when trying to stop the connection.");
+        }
+        Log.i("Info", "Connection closed successfully.");
     }
 }
